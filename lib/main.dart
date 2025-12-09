899198cd7f3fe7c2121096377aec6ab54954db02
adityasahare90-creator <aditya.sahare90@gmail.com>
2025-12-10 00:21:13 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ShineApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ShineApp extends StatelessWidget {
+  const ShineApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Shine28',
+      title: 'Shine 28',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: const Text('Shine28')),
+        appBar: AppBar(
+          title: const Text('Shine 28'),
+        ),
         body: const Center(
           child: Text(
-            'Shine28 App Running ✅',
+            'Shine 28 App is Running ✅',
             style: TextStyle(fontSize: 20),
           ),
         ),
